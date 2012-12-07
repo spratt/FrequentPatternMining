b57b664986b4538b01b22cfdbad2b1aded8f773a
Simon Pratt <spratt@scs.carleton.ca>
2012-12-06 21:52:45 -0500

    Added dataset class, license, and copyright files

--- code/dataset.py
+++ code/dataset.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python2.6
+######################################################################
+# dataset.py
+######################################################################
+# In which we define a Dataset object which stores rows of itemsets
+# and can parse information from a file which separates itemsets by
+# newlines and separates items within an itemset by a single space.
+######################################################################
+# For license information, see LICENSE file
+# For copyright information, see COPYRIGHT file
+######################################################################
 
 class Dataset(object):
     def __init__(self):
@@ -12,6 +22,9 @@ class Dataset(object):
             canonical = line.strip().lower()
             self.rows.append(canonical.split(" "))
 
+######################################################################
+# Basic Tests
+######################################################################
 if __name__ == '__main__':
 
     import sys
@@ -26,3 +39,4 @@ if __name__ == '__main__':
         ds.readFromFile(f)
         
     print "Read {0} lines in {1}".format(len(ds),filename)
+    print ds.rows[0]
